21aeba988f15fe709cad131dca10777ce75a1f9e
arispretz <117314460+arispretz@users.noreply.github.com>
2024-07-03 22:09:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 const UNSPLASH_ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://https://image-search-abstra-git-47aa11-ariana-carolina-spretzs-projects.vercel.app' 
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
